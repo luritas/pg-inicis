1f908c40a8a77cacff3d47ef611ca91447170c8b
programmer <visualplus@naver.com>
2015-12-29 03:21:37 +0900

    test용으로 업로드

--- views/mobile/pay_request.blade.php
+++ views/mobile/pay_request.blade.php
@@ -5,9 +5,6 @@
 <body onload="formSubmit();">
     <form id="form1" name="form1" method="POST" action="{{ $targetUrl }}" accept-charset="euc-kr">
         @foreach ($dataField as $name => $data)
-            <?php
-            $value = mb_convert_encoding($data, "EUC-KR", "UTF-8");
-            ?>
             <input type="hidden" name="<?php echo $name?>" value="<?php echo str_replace('"', '', $data)?>">
         @endforeach
     </form>
